c093959b27e7565c187dc810f9219c74f9d023ff
Ashutosh <ashutoshpatel@yahoo.in>
2023-08-26 18:14:23 +0530

    FIx: Code

--- src/main/java/info/ashutosh/lambda/scenario3/Logic.java
+++ src/main/java/info/ashutosh/lambda/scenario3/Logic.java
@@ -27,10 +27,11 @@ public class Logic {
 			}
 			return in;
 		}).collect(Collectors.toList());
-		// map
-		List<String> collect2 = list.stream()..map(in -> in.equals("A") ? in.concat("A") : in)
-		}).collect(Collectors.toList());
 		System.out.println(collect2);
+		// map
+		List<String> collectx = list.stream().map(in -> in.equals("A") ? in.concat("A") : in)
+				.collect(Collectors.toList());
+		System.out.println(collectx);
 
 		List<String> collect3 = list.stream().distinct().collect(Collectors.toList());
 		System.out.println(collect3);
