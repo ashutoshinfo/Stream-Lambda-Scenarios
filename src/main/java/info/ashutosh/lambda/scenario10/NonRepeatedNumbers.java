3df222a065edc3de724d58e063b8162b7fd6b101
Ashutosh <ashutoshpatel@yahoo.in>
2023-08-29 03:18:57 +0530

    Fix: find Duplicates using Simple java

--- src/main/java/info/ashutosh/lambda/scenario10/NonRepeatedNumbers.java
+++ src/main/java/info/ashutosh/lambda/scenario10/NonRepeatedNumbers.java
@@ -1,7 +1,10 @@
 package info.ashutosh.lambda.scenario10;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -11,17 +14,55 @@ public class NonRepeatedNumbers {
 	public static void main(String[] args) {
 		List<Integer> asList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 1);
 
-		// 1st way
-		Set<Integer> integers = new HashSet<>();
-//		System.out.println(integers);
-//
-//		for (Integer integer : asList) {
-//			integers.add(integer);
-//		}
+		List<Integer> findDuplicatesLmbda = findDuplicates(asList);
+		System.out.println(findDuplicatesLmbda);
 
-		 asList.stream().filter(in -> integers.add(in)).forEach(System.out::println);
+		Set<Integer> findDuplicates2nd = findDuplicates2nd(asList);
+		System.out.println(findDuplicates2nd);
 
-//		System.out.println(collect);
+		List<Integer> collect = findDuplicatesLmbda(asList);
+		System.out.println(collect);
 
+	}
+
+	private static Set<Integer> findDuplicates2nd(List<Integer> asList) {
+
+		Set<Integer> nonDuplicates = new HashSet<>();
+		Set<Integer> duplicates = new HashSet<>();
+
+		for (Integer integer : asList) {
+			if (!nonDuplicates.add(integer)) {
+				duplicates.add(integer);
+			}
+
+		}
+
+		ArrayList<Integer> arrayList = new ArrayList<>(duplicates);
+		Collections.sort(arrayList, (a, b) -> -a.compareTo(b));
+		Set<Integer> fSet = new LinkedHashSet<>(arrayList);
+		return fSet;
+
+	}
+
+	private static List<Integer> findDuplicates(List<Integer> asList) {
+		List<Integer> duplicates = new ArrayList<>();
+		List<Integer> distinctNumbers = new ArrayList<>();
+
+		for (Integer number : asList) {
+			if (distinctNumbers.contains(number)) {
+				if (!duplicates.contains(number)) {
+					duplicates.add(number);
+				}
+			} else {
+				distinctNumbers.add(number);
+			}
+		}
+		return duplicates;
+	}
+
+	private static List<Integer> findDuplicatesLmbda(List<Integer> asList) {
+		Set<Integer> integersLambda = new HashSet<>();
+		List<Integer> collect = asList.stream().filter(in -> !integersLambda.add(in)).collect(Collectors.toList());
+		return collect;
 	}
 }
